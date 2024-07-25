29008e3d574630e25058c24f447e2373c53d0b76
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-07-25 01:56:00 -0700

    Create Navbar.vue

---
+++ Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">DataViz</div>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background: linear-gradient(90deg, #2193b0, #6dd5ed);
+  color: #fff;
+}
+.navbar .logo {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+.navbar ul {
+  list-style: none;
+  display: flex;
+}
+.navbar ul li {
+  margin-left: 1rem;
+}
+.navbar ul li a {
+  text-decoration: none;
+  color: #fff;
+}
+</style>
