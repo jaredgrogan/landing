5f88f5ff579d9e8c8f65bf13e341420c322cc5ed
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-07-25 01:58:27 -0700

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Arial', sans-serif;
+  background: #f5f5f5;
+  color: #333;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
