a401e3fb357fc7fe757a882c38a28361e173f3fd
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-07-26 00:08:52 -0700

    Create scriptnight.js

---
+++ scriptnight.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+    const body = document.body;
+    const modeText = document.getElementById('mode-text');
+    if (body.classList.contains('night-mode')) {
+        body.classList.remove('night-mode');
+        body.classList.add('day-mode');
+        modeText.textContent = 'day';
+    } else {
+        body.classList.remove('day-mode');
+        body.classList.add('night-mode');
+        modeText.textContent = 'night';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modeToggle = document.getElementById('modeToggle');
+    const body = document.body;
+
+    modeToggle.addEventListener('click', () => {
+        body.classList.toggle('night-mode');
+        body.classList.toggle('day-mode');
+    });
+});
+
+function launchApp() {
+    window.location.href = 'app.html';
+}
