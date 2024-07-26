451649500be44da43515d9cdce0dedf536dc9063
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-07-26 00:12:47 -0700

    Update scriptnight.js

--- scriptnight.js
+++ scriptnight.js
@@ -15,11 +15,24 @@ function toggleMode() {
 document.addEventListener('DOMContentLoaded', () => {
     const modeToggle = document.getElementById('modeToggle');
     const body = document.body;
+    const typingAnimation = document.getElementById('typingAnimation');
+    const text = "Welcome to Universitas Pro";
+    let index = 0;
 
     modeToggle.addEventListener('click', () => {
         body.classList.toggle('night-mode');
         body.classList.toggle('day-mode');
     });
+
+    function typeWriter() {
+        if (index < text.length) {
+            typingAnimation.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeWriter, 100);
+        }
+    }
+
+    typeWriter();
 });
 
 function launchApp() {
