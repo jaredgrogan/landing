7d5f845f5c6d513148221b7a831459a7225c1cde
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-07-31 03:10:07 -0700

    Update innovation8.js

--- innovation8.js
+++ innovation8.js
@@ -11,7 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const updateDateTime = () => {
         const now = new Date();
-        const formattedDate = now.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+        const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedDate = now.toLocaleDateString('en-US', options);
         const formattedTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
         dateTimeElement.textContent = `${formattedDate}, ${formattedTime}`;
