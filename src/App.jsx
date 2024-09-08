86c784ef788feec52466b0936c8d5cf01efdfc60
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-09-08 10:15:02 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Universitas Pro</h1>
+      {/* Add your components here */}
+    </div>
+  )
+}
+
+export default App
