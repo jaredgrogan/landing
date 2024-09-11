26f2cfb9a736ee8f2ab8ed1a9df6d771eb6ef40b
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-09-10 19:55:06 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
+   const { createProxyMiddleware } = require('http-proxy-middleware');
+   const app = express();
 
-app.use('/api/proxy', createProxyMiddleware({
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api/proxy': '',
-  },
-  onProxyReq: (proxyReq, req) => {
-    const url = req.query.url;
-    if (url) {
-      proxyReq.path = url;
-    }
-  },
-}));
+   app.use('/api/proxy', createProxyMiddleware({
+     changeOrigin: true,
+     pathRewrite: {
+       '^/api/proxy': '',
+     },
+     onProxyReq: (proxyReq, req) => {
+       const url = req.query.url;
+       if (url) {
+         proxyReq.path = url;
+       }
+     },
+   }));
 
-module.exports = app;
+   module.exports = app;
+   
