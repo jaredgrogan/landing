bed6822e31d6685e562402b1352bada9f0ac71b4
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-09-10 19:12:04 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,13 @@
-// /api/proxy.js
 const express = require('express');
 const request = require('request');
 const app = express();
 
 app.get('/proxy', (req, res) => {
     const url = req.query.url;
-
     if (!url) {
         return res.status(400).send('No URL provided');
     }
-
-    // Use 'request' to fetch the URL content and pipe it back
+    // Fetch the URL content and stream it back to the client
     request(url).pipe(res);
 });
 
