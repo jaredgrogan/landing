d44538eb8a00de5c703493af5516d1615fc2439f
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-09-10 20:08:44 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,19 +1,26 @@
 const express = require('express');
-   const { createProxyMiddleware } = require('http-proxy-middleware');
-   const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-   app.use('/api/proxy', createProxyMiddleware({
-     changeOrigin: true,
-     pathRewrite: {
-       '^/api/proxy': '',
-     },
-     onProxyReq: (proxyReq, req) => {
-       const url = req.query.url;
-       if (url) {
-         proxyReq.path = url;
-       }
-     },
-   }));
+const app = express();
 
-   module.exports = app;
-   
+app.use('/', createProxyMiddleware({
+  target: 'http://example.com', // Replace with a default target if needed
+  changeOrigin: true,
+  router: (req) => {
+    const url = new URL(req.query.url);
+    return url.origin;
+  },
+  pathRewrite: (path, req) => {
+    const url = new URL(req.query.url);
+    return url.pathname + url.search;
+  },
+  onError: (err, req, res) => {
+    console.error('Proxy Error:', err);
+    res.writeHead(500, {
+      'Content-Type': 'text/plain',
+    });
+    res.end('Proxy error: ' + err.message);
+  }
+}));
+
+module.exports = app;
