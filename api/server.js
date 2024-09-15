514d145ec72dfd83a54a0699b0b4a983eb479628
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-09-15 15:15:31 -0700

    Create server.js

---
+++ api/server.js
@@ -0,0 +1,8 @@
+// api/server.js
+
+export default async function handler(req, res) {
+  // Your server logic here
+
+  // Example response
+  res.status(200).json({ message: 'Hello from server.js!' });
+}
